3787bad1279ffd20e669673e4f3180978d91b611
jccintr <jccintr@gmail.com>
2022-11-23 00:56:33 -0300

    apos rect-icons

--- src/Components/navbar/NavBar.jsx
+++ src/Components/navbar/NavBar.jsx
@@ -1,20 +1,21 @@
 import React, {useState} from 'react'
 import styles from './styles.module.css';
+import { FaBars,FaTimes } from "react-icons/fa";
 
 const NavBar = () => {
- const [isMobile,setIsmobile] = useState(false);
+ const [showMenuMobile,setShowMenuMobile] = useState(false);
 
   return (
     <nav className={styles.navbar}>
       <h3 className={styles.logo}>SEU LOGO</h3>
-      <ul  className={isMobile ? styles.navlinksmobile : styles.navlinks}>
+      <ul  className={showMenuMobile ? styles.navlinksmobile : styles.navlinks}>
         <li className={styles.links}>Home</li>
         <li className={styles.links}>Portfolio</li>
         <li className={styles.links}>Serviços</li>
         <li className={styles.links}>Contato</li>
       </ul>
-      <button className={styles.mobilemenuicon}onClick={()=>setIsmobile(!isMobile)}>
-        {isMobile ? <p>x</p>:<p>H</p>}
+      <button className={styles.mobilemenuicon} onClick={()=>setShowMenuMobile(!showMenuMobile)}>
+        {showMenuMobile ? <FaTimes/>:<FaBars/>}
       </button>
     </nav>
   )
